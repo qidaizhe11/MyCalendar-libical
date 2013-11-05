6b9c946785d384bd2d83ffe7bf65624f5c4db176
Daizhe <qidaizhe11@gmail.com>
2013-11-05 23:44:00 +0800

    CalendarModel .cpp文件 日历的基础框架copy完毕
    大师的rideModel也太复杂了，完全跟不上唉，囧

--- src/calendarmodel.cpp
+++ src/calendarmodel.cpp
@@ -52,8 +52,7 @@ void CalendarModel::setSourceModel(QAbstractItemModel *sourceModel)
             this, SLOT(refresh()));
     connect(sourceModel, SIGNAL(rowsRemoved(QModelIndex,int,int)),
             this, SLOT(refresh()));
-    // TODO:
-    //connect(context->athlete->rideCalendar
+    // TODO: connect(context->athlete->rideCalendar
     refresh();
 }
 
@@ -93,6 +92,111 @@ Qt::ItemFlags CalendarModel::flags(const QModelIndex &index) const
     return Qt::ItemIsSelectable | Qt::ItemIsEnabled;
 }
 
+QVariant CalendarModel::data(const QModelIndex &proxyIndex, int role) const
+{
+    if (!proxyIndex.isValid())
+        return QVariant();
+
+    QVariant returning;
+
+    switch (role) {
+    case Qt::BackgroundRole:
+    {
+        QList<QColor> colors;
+        QVector<int> *arr = dateToRows.value(date(proxyIndex), NULL);
+        if (arr) {
+            foreach (int i, *arr) {
+                // TODO: if its the current ride
+                //
+
+                colors << QColor(sourceModel()->data(
+                            index(i, colorIndex, QModelIndex())).toString());
+            }
+        }
+
+        return QVariant::fromValue<QList<QColor> >(colors);
+        break;
+    }
+
+    case Qt::ForegroundRole:
+    {
+        QList<QColor> colors;
+        QVector<int> *arr = dateToRows.value(date(proxyIndex), NULL);
+        if (arr) {
+            foreach (int i, *arr) {
+                QString fileName = sourceModel()->data(
+                            index(i, filenameIndex, QModelIndex())).toString();
+                colors << QColor(Qt::black);
+            }
+        }
+
+        return QVariant::fromValue<QList<QColor> >(colors);
+        break;
+    }
+
+    case Qt::EditRole:
+    case Qt::DisplayRole:  // returns the string to display
+    {
+        QVector<int> *arr = dateToRows.value(date(proxyIndex), NULL);
+        QStringList strings;
+        if (arr) {
+            foreach (int i, *arr) {
+                strings << sourceModel()->data(
+                               index(i, textIndex, QModelIndex())).toString();
+            }
+        }
+
+        return strings;
+        break;
+    }
+
+        // how should this date be described in the cell heading
+    case DateStringRole:
+    {
+        QDate today = date(proxyIndex);
+        if (today.month() != month &&
+            (today.addDays(1).month() == month || today.addDays(-1).month()
+                == month)) {
+            return QString("%1 %2").arg(today.day()).arg(
+                        QDate::shortMonthName(today.month()));
+        }
+        else {
+            return QString("%1").arg(today.day());
+        }
+        break;
+    }
+    case DayRole:
+    {
+        QDate today = date(proxyIndex);
+        if (today.month() != month &&
+            (today.addDays(1).month() == month || today.addDays(-1).month()
+                == month))
+            return "";
+        else
+            return QString("%1").arg(today.day());
+        break;
+    }
+    default:
+        return QVariant();
+        break;
+    }
+    return QVariant();
+}
+
+QVariant CalendarModel::headerData(int section, Qt::Orientation orientation,
+                                   int role) const
+{
+    if (role == Qt::DisplayRole) {
+        if (orientation == Qt::Horizontal) {
+            return QDate::shortDayName(section + 1);
+        }
+        else {
+            return QString("%1").arg(date(index(section, 0)).weekNumber());
+        }
+    }
+    return QVariant();
+}
+
 //
 // slots
 //
@@ -119,4 +223,119 @@ void CalendarModel::refresh()
     rows = ndays / 7;
 
     reset();
+}
+
+//-------------------------------------------------------------------------
+//
+// CalendarDelegate
+
+CalendarDelegate::CalendarDelegate(QTableView *parent)
+    : QItemDelegate(parent)
+{
+}
+
+void CalendarDelegate::createPainterPath(
+        QPainterPath &emptyPath, const QRect &fullItemRect,
+        const int iRoundTop, const int iRoundBottom) const
+{
+    emptyPath = QPainterPath();
+    bool bRoundTop = iRoundTop > 0;
+    bool bRoundBottom = iRoundBottom > 0;
+
+    if (bRoundTop) {
+        emptyPath.moveTo(fullItemRect.topLeft() + QPoint(0, iRoundTop));
+        emptyPath.quadTo(fullItemRect.topLeft(), fullItemRect.topLeft() +
+                         QPoint(iRoundTop, 0));
+    }
+    else {
+        emptyPath.moveTo(fullItemRect.topLeft());
+    }
+
+    emptyPath.lineTo(fullItemRect.topRight() - QPoint(iRoundTop, 0));
+
+    if (bRoundTop)
+        emptyPath.quadTo(fullItemRect.topRight(),
+                         fullItemRect.topRight() + QPoint(0, iRoundTop));
+
+    emptyPath.lineTo(fullItemRect.bottomRight() - QPoint(0, iRoundBottom));
+
+    if (bRountBottom)
+        emptyPath.quadTo(fullItemRect.bottomRight(),
+                         fullItemRect.bottomRight() - QPoint(iRoundBottom, 0));
+
+    emptyPath.lineTo(fullItemRect.bottomLeft() + QPoint(iRoundBottom, 0));
+
+    if (bRountBottom)
+        emptyPath.quadTo(fullItemRect.bottomLeft(),
+                         fullItemRect.bottomLeft() - QPoint(0, iRoundBottom));
+
+    emptyPath.closeSubpath();
+}
+
+void CalendarDelegate::paint(QPainter *painter,
+        const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    painter->save();
+
+    // font
+    QVariant vfont = index.data(Qt::FontRole);
+    painter->setFont(vfont.value<QFont>());
+
+    // cell decoration
+    QColor bg = index.data(CalendarModel::CellColorRole).value<QColor>();
+    QColor hg = index.data(CalendarModel::HeaderColorRole).value<QColor>();
+
+    // if selected then use selection color anyway
+    if (option.state & QStyle::State_Selected)
+        bg = option.palette.highlight().color();
+    painter->fillRect(option.rect, bg);
+
+    // still paint header
+    QRect hd(option.rect.x(), option.rect.y(), option.rect.width(), 15);
+    painter->fillRect(hd, hg);
+
+    // date...
+    QString dateString = index.data(CalendarModel::DateStringRole).toString();
+    QTextOption textOption(Qt::AlignRight);
+    painter->drawText(hd, dateString, textOption);
+
+    // text
+    QStringList texts = index.data(Qt::DisplayRole).toStringList();
+    QList<QColor> colors =
+            index.data(Qt::BackgroundRole).value<QList<QColor> >();
+    QList<QColor> pens =
+            index.data(Qt::ForegroundRole).value<QList<QColor> >();
+    if (texts.count()) {
+        int height = (option.rect.height()-21) / texts.count();
+        int y = option.rect.y() + 17;
+        int i = 0;
+        foreach (QString text, texts) {
+            QRect bd(option.rect.x()+2, y, option.rect.width() - 4, height);
+            y += height + 2;
+
+            QPainterPath cachePath;
+            createPainterPath(cachePath, bd, 4, 4);
+
+            QPen pen;
+            QColor color = pens[i];
+            pen.setColor(color);
+
+            // if not black use it for border
+            if (color != QColor(Qt::black))
+                painter->setPen(pen);
+            else
+                painter->setPen(Qt::NoPen);
+            QColor fillColor = colors[i++];
+            fillColor.setAlpha(200);
+            painter->setBrush(fillColor);
+            painter->setRenderHint(QPainter::Antialiasing);
+            painter->drawPath(cachePath);
+
+            // text always needs a pen
+            painter->setPen(pen);
+            painter->drawText(bd, text);
+        } // end foreach
+    } // end if
+
+    painter->restore();
 }
